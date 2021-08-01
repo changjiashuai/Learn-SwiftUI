105cce52d64d5cb93cd29a0ce04b4b3f2fef4642
changjiashuai <changjiashuai@gmail.com>
2021-08-01 17:13:10 +0800

    Initial Commit

---
+++ Shared/Learn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUIApp.swift
+//  Shared
+//
+//  Created by CJS on 2021/8/1.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
